ba0b4e99e74f2e38ee57a7849e657bebf741d19e
Gabriel Desidério <gabrieldesd7@gmail.com>
2024-12-15 12:51:07 -0300

    create login page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,13 +1,34 @@
 import { Helmet } from 'react-helmet-async'
-import { Link } from 'react-router'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 
 export function SignIn() {
   return (
-    <div>
+    <>
       <Helmet title="Login" />
-
-      <h1>Sign IN</h1>
-      <Link to="/">Dashboard</Link>
-    </div>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-1 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </p>
+          </div>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" name="email" type="email" />
+            </div>
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
