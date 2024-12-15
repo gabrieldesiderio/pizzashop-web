51e161b9cfa457d30e3cc6e51df615ee6ceb5890
Gabriel Desidério <gabrieldesd7@gmail.com>
2024-12-15 15:06:10 -0300

    theme toggle and account menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import './global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { AppRoutes } from './routes'
 
 export function App() {
   return (
-    <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <AppRoutes />
-    </HelmetProvider>
+    <ThemeProvider storageKey="pizzashop-theme">
+      <HelmetProvider>
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <AppRoutes />
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
