b1a07f4ba0bbe7472cc6968270cef31704dc0c22
Gabriel Desidério <gabrieldesd7@gmail.com>
2024-12-15 12:31:15 -0300

    setup layouts on react router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,20 @@
 import { Route, Routes } from 'react-router'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route index element={<Dashboard />} />
-      <Route path="sign-in" element={<SignIn />} />
+      <Route element={<AuthLayout />}>
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
+
+      <Route element={<AppLayout />}>
+        <Route index element={<Dashboard />} />
+      </Route>
     </Routes>
   )
 }
