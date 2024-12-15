72a5937e953f90c94bc116dc53607c8fca944943
Gabriel Desidério <gabrieldesd7@gmail.com>
2024-12-15 14:39:12 -0300

    create sign-up page

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,12 +4,14 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route element={<AuthLayout />}>
         <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
       </Route>
 
       <Route element={<AppLayout />}>
